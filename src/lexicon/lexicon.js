e107a40065c209a13c1eb0c74cfb2a4d2e58b82c
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-08-29 13:11:48 -0400

    feat(dictionary): merriam-webster definitions support

--- src/lexicon/lexicon.js
+++ src/lexicon/lexicon.js
@@ -1,3 +1,50 @@
+
+const defined_word = (word) => { return `${word}`}
+const part_of_speech = (part_of_speech) => { return `${part_of_speech.toLowerCase()},` }
+const word_break = (word) => { return `${word}`}
+const date_first_used = (word) => { return `${word}`}
+const etymology = (word) => { return `◦ ${word}`}
+const pronunciation = (ox_pronounce, mer_pronounce) => { return `\\ ${ox_pronounce} \\ ${mer_pronounce} \\` }
+const main_definitions = (mer_def_array, ox_def_array) => {
+    const ol = document.getElementById("definition_list");
+    mer_def_array.forEach((definition) => {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(definition));
+        ol.appendChild(li);
+    }, ol)
+    ox_def_array.forEach((definition) => {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(definition));
+        ol.appendChild(li);
+    }, ol)
+}
+const word_examples = (example) => { return `e.g. ${example}` }
+
+const stems = (stem_array) => {
+    let stem_concat = '';
+    stem_array.forEach((item) => {
+        stem_concat += `${item}, `
+    });
+
+    return `${stem_concat}`
+}
+
+const word_audio = (word_audio) => {
+    const audio = document.getElementById('audio');
+    if (audio.classList.contains('no_display')){
+        audio.classList.remove('no_display')
+    }
+    const source = document.getElementById('audioSource');
+    source.src = word_audio;
+    audio.load();
+}
+
+const click_search = (event) => {
+    if (event.keyCode === 13) {
+        send_search_word()
+    }
+}
+
 const switch_loader = () => {
     const load_visibility = document.getElementById('load_visibility')
     const search_visibility = document.getElementById('search_visibility')
@@ -15,40 +62,20 @@ const switch_loader = () => {
     }
 }
 
-const defined_word = (word_def) => {
-    return `${word_def['word']} `
-}
+const set_definition_elements = (word_def) =>{
+    const oxford_word_def = word_def['oxford']
+    const merriam_word_def = word_def['merriam_webster']
+    word_audio(oxford_word_def['audio'])
+    set_element_inner_text('word', defined_word(oxford_word_def['word']))
+    set_element_inner_text('part_of_speech', part_of_speech(oxford_word_def['part_of_speech']))
+    set_element_inner_text('word_break', word_break(merriam_word_def['word_break']))
+    set_element_inner_text('pronunciation', pronunciation(oxford_word_def['pronounce'], merriam_word_def['pronounce']))
+    set_element_inner_text('date_first_used', date_first_used(merriam_word_def['date_first_used']))
+    set_element_inner_text('etymology', etymology(merriam_word_def['etymology']))
+    set_element_inner_text('stems', stems(merriam_word_def['stems']))
 
-const part_of_speech = (word_def) => {
-    return `${word_def['part_of_speech'].toLowerCase()}`
-}
-
-const pronunciation = (word_def) => {
-    return `\\ ${word_def['pronounce']} \\`
-}
-
-const main_definitions = (word_def) => {
-    return `◦ ${word_def['definition']}`
-}
-
-const word_audio = (word_def) => {
-    const audio = document.getElementById('audio');
-    if (audio.classList.contains('no_display')){
-        audio.classList.remove('no_display')
-    }
-    const source = document.getElementById('audioSource');
-    source.src = word_def['audio'];
-    audio.load();
-}
-
-const word_examples = (word_def) => {
-    return `e.g. ${word_def['example']}`
-}
-
-const click_search = (event) => {
-    if (event.keyCode === 13) {
-        send_search_word()
-    }
+    main_definitions(merriam_word_def['definition'], oxford_word_def['definition'])
+    set_element_inner_text('word_examples', word_examples(oxford_word_def['example']))
 }
 
 const send_search_word = () => {
@@ -58,14 +85,9 @@ const send_search_word = () => {
     fetch(`${host}:8000/lexicon/word_search/${inputVal.toLocaleString().toLowerCase()}`)
         .then(response => response.json())
         .then(response_container => {
+            console.log(response_container['definition'])
             const word_def = response_container['definition']
-            console.log(word_def)
-            word_audio(word_def)
-            set_element_inner_text('word', defined_word(word_def))
-            set_element_inner_text('part_of_speech', part_of_speech(word_def))
-            set_element_inner_text('pronunciation', pronunciation(word_def))
-            set_element_inner_text('main_definitions', main_definitions(word_def))
-            set_element_inner_text('word_examples', word_examples(word_def))
+            set_definition_elements(word_def)
             switch_loader()
         })
 }
