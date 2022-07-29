b3ddfdece3f6d8287c0d5e49acb52a1381580f11
roboto84 <7658704+roboto84@users.noreply.github.com>
2022-07-29 08:30:05 -0400

    feat(theme): accept theme setting as URL param

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -7,11 +7,20 @@ export const setLocalStorage = (key: string, value: any) => {
 };
 
 export const getInitialTheme = ():ThemeModeEnum => {
-  const storageTheme: string = getLocalStorage(LocalStorageEnum.THEME);
-
-  if (storageTheme) {
-    return storageTheme === 'LIGHT' ? ThemeModeEnum.LIGHT : ThemeModeEnum.DARK;
+  const urlParams:URLSearchParams = new URLSearchParams(window.location.search);
+  const theme:string|null = urlParams.get('theme');
+  if (theme) {
+    if (theme && theme === ThemeModeEnum.DARK) {
+      return ThemeModeEnum.DARK;
+    }
+    return ThemeModeEnum.LIGHT;
   }
+
+  const storageTheme: string = getLocalStorage(LocalStorageEnum.THEME);
+  if (storageTheme) {
+    return storageTheme === ThemeModeEnum.LIGHT ? ThemeModeEnum.LIGHT : ThemeModeEnum.DARK;
+  }
+
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     return ThemeModeEnum.DARK;
   }
